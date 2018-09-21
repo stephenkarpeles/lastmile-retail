cab72d9ab1b8806c9de95ad98e0bd132009718c5
Stephen Karpeles <skarpeles@bluechipww.com>
2018-09-20 23:22:47 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Lastmile Retail</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="manifest" href="site.webmanifest">
+    <link rel="apple-touch-icon" href="icon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+
+  <body>
+    <section>
+      <div class="container">
+        <p>Hello world! This is Lastmile Retail.</p>
+      </div>
+    </section>
+
+    <!-- Scripts -->  
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+    <script>
+      window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+      ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+    </script>
+    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+
+  </body>
+  
+</html>
