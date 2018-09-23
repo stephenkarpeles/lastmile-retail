9b8d76d1193b20a44c1cf5a17c7e8fbf8ec53702
Stephen Karpeles <skarpeles@bluechipww.com>
2018-09-23 14:14:28 -0500

    Desktop footer setup

--- partials/footer.php
+++ partials/footer.php
@@ -1,3 +1,32 @@
+		</main>
+
+		<footer class="footer">
+			<div class="footer__container">
+				<div class="footer__copyright">
+					&copy; Copyright Lastmile Retail, Inc <?php echo date('Y'); ?>. All Rights Reserved.
+				</div>
+
+				<div class="footer__address">
+					<span>300 Park Avenue, New York, NY 10022</span>
+				</div>
+
+				<div class="footer__right-block">
+					<div class="footer__social-menu">
+						<ul>
+							<li><a href="">Email</a></li>
+							<li><a href="">FB</a></li>
+							<li><a href="">Twitter</a></li>
+							<li><a href="">G+</a></li>
+						</ul>
+					</div>
+
+					<div class="footer__cta">
+						<button>Demo</button>
+					</div>
+				</div>
+			</div>
+		</footer>	  
+
 	  <!-- Scripts -->  
 	  <script src="js/main.js"></script>
 
