5afca185688c6af67ba873f52f763a7dab70dd18
Stephen Karpeles <skarpeles@bluechipww.com>
2018-09-22 22:14:12 -0500

    Setting up header

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
-<!doctype html>
-<html class="no-js" lang="">
+<?php include 'partials/header.php'; ?>
 
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Lastmile Retail</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <header class="header">    
+    <div class="header__container">
 
-    <link rel="manifest" href="site.webmanifest">
-    <link rel="apple-touch-icon" href="icon.png">
-    <!-- Place favicon.ico in the root directory -->
-
-    <link rel="stylesheet" href="css/style.css">
-  </head>
-
-  <body>
-    <section>
-      <div class="container">
-        <p>Hello world! This is Lastmile Retail.</p>
+      <div class="header__tagline">
+        <span>From Browse to Buy</span>
       </div>
-    </section>
 
-    <!-- Scripts -->  
-    <script src="js/main.js"></script>
+      <div class="header__logo">
+        <img src="img/logo.svg" alt="Lastmile Retail">
+      </div>
 
-    <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
-    <script>
-      window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
-      ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
-    </script>
-    <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+      <div class="header__right-block">
+        <div class="header__page-marker">
+          <span>Home</span>
+        </div>
 
-  </body>
-  
-</html>
+        <div class="header__menu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>   
+      </div> 
+
+    </div>
+  </header>
+
+  <section>
+    <div class="container">
+      <p>Hello world! This is Lastmile Retail.</p>
+    </div>
+  </section>
+
+<?php include 'partials/footer.php'; ?>
+
+    
