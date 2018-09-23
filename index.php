da571ee7c8b64996cf9b949ef101d991d0647863
Stephen Karpeles <skarpeles@bluechipww.com>
2018-09-22 22:42:52 -0500

    Lots of header stylings and variables

--- index.php
+++ index.php
@@ -1,34 +1,8 @@
 <?php include 'partials/header.php'; ?>
 
-  <header class="header">    
-    <div class="header__container">
-
-      <div class="header__tagline">
-        <span>From Browse to Buy</span>
-      </div>
-
-      <div class="header__logo">
-        <img src="img/logo.svg" alt="Lastmile Retail">
-      </div>
-
-      <div class="header__right-block">
-        <div class="header__page-marker">
-          <span>Home</span>
-        </div>
-
-        <div class="header__menu">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>   
-      </div> 
-
-    </div>
-  </header>
-
-  <section>
+  <section class="hero hero--home">
     <div class="container">
-      <p>Hello world! This is Lastmile Retail.</p>
+      <h1>unlock your local advantage</h1>
     </div>
   </section>
 
--- partials/header.php
+++ partials/header.php
@@ -16,3 +16,29 @@
   </head>
 
   <body>
+
+    <header class="header">    
+      <div class="header__container">
+
+        <div class="header__tagline">
+          <span>From Browse to Buy</span>
+        </div>
+
+        <div class="header__logo">
+          <img src="img/logo.svg" alt="Lastmile Retail">
+        </div>
+
+        <div class="header__right-block">
+          <div class="header__page-marker">
+            <span>Home</span>
+          </div>
+
+          <div class="header__menu">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>   
+        </div> 
+
+      </div>
+    </header>
