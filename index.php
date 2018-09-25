3f9756c5cb33888e0cfb079ee76378bb48deff8c
Stephen Karpeles <skarpeles@bluechipww.com>
2018-09-24 22:23:31 -0500

    Adding homepage content

--- index.php
+++ index.php
@@ -6,7 +6,23 @@
         <h1>unlock your local advantage online</h1>
         <p>Lastmile bridges the gap between the 80% of shoppers that research online and the 90% of sales that happen offline.</p>
       </div>
+      <div class="hero__icon">
+      	<img src="img/icon-computer-home.svg" alt="Computer to Store">
+      </div>
     </div>
+  </section>
+
+  <section class="intro-wrap intro-wrap--home">
+  	<div class="container">
+  		<p class="intro intro--home">Boost in-store and online sales by aligning your digital presence with how today's consumers actually shop.</p>
+  	</div>
+  </section>
+
+  <section>
+  	<div class="container">
+  		<h2>Local Motive&trade; Omni-Channel Platform</h2>
+  		<p><strong>Lastmile's Local Motive&trade;</strong> platform unlocks brick and mortar’s hidden growth potential by reaching online shoppers at the moment they show local intent increasing both online and in-store buying. We have everything you need to create a leading omni-channel shopper experience online.</p>
+  	</div>
   </section>
 
 <?php include 'partials/footer.php'; ?>
