a56b10b6011639a1a76c7d0740eb7ccce9a7c3d5
Stephen Karpeles <skarpeles@bluechipww.com>
2018-09-30 22:26:24 -0500

    Mobile/tablet shopper journey updates

--- shopper-journey.php
+++ shopper-journey.php
@@ -48,17 +48,19 @@
     </div>
   </section>
 
-  <section class="no-top-padding">
+  <section class="no-padding">
     <div class="container">
       <h2 class="heading-alt heading-alt--secondary heading-alt--tight">Our Proprietary Methodology</h2>
       <p><strong>"Heart, Head, Hands and Voice</strong> approach first connects on an emotional level, then provides shoppers with the information and tools to make purchasing satisfying and easy.</p>
     </div>
   </section>
 
-  <section class="shopper-journey-chart">
+  <section class="shopper-journey-chart no-top-padding">
 
     <div class="shopper-journey-chart__column">
-      <img src="img/icons/icon-tag-filled.svg" alt="Icon">
+      <div class="shopper-journey-chart__img-wrap">
+        <img src="img/icons/icon-tag-filled.svg" alt="Icon">
+      </div>
       <div class="shopper-journey-chart__content">
         <h3>Give Shoppers an Excuse to Buy</h3>
         <p>We matching both the context and intent of the shopper through thoughtful messaging and placement that speaks directly to their local needs.</p>
@@ -66,32 +68,38 @@
     </div> 
 
     <div class="shopper-journey-chart__column">
-      <img src="img/icons/icon-tag-filled.svg" alt="Icon">
+      <div class="shopper-journey-chart__img-wrap">
+        <img src="img/icons/icon-user.svg" alt="Icon">
+      </div>
       <div class="shopper-journey-chart__content">
-        <h3>Give Shoppers an Excuse to Buy</h3>
-        <p>We matching both the context and intent of the shopper through thoughtful messaging and placement that speaks directly to their local needs.</p>
+        <h3>Give Shoppers a Reason to Buy</h3>
+        <p>We can now arm shoppers with relevant content and creative such as local stock status, pricing, promotions and product details on the landing pages and experiences that provide.</p>
       </div>
     </div> 
 
     <div class="shopper-journey-chart__column">
-      <img src="img/icons/icon-tag-filled.svg" alt="Icon">
+      <div class="shopper-journey-chart__img-wrap">
+        <img src="img/icons/icon-omni-channel.svg" alt="Icon">
+      </div>
       <div class="shopper-journey-chart__content">
-        <h3>Give Shoppers an Excuse to Buy</h3>
-        <p>We matching both the context and intent of the shopper through thoughtful messaging and placement that speaks directly to their local needs.</p>
+        <h3>Give Shoppers an Easy Way to Buy</h3>
+        <p>We arm shoppers with the tools they need to decide and commit. We remove any roadblocks or friction from the process with both online and offline buying options.</p>
       </div>
     </div> 
 
     <div class="shopper-journey-chart__column">
-      <img src="img/icons/icon-tag-filled.svg" alt="Icon">
+      <div class="shopper-journey-chart__img-wrap">
+        <img src="img/icons/icon-phone.svg" alt="Icon">
+      </div>
       <div class="shopper-journey-chart__content">
-        <h3>Give Shoppers an Excuse to Buy</h3>
-        <p>We matching both the context and intent of the shopper through thoughtful messaging and placement that speaks directly to their local needs.</p>
+        <h3>Give Shoppers a Way to Advocate</h3>
+        <p>We craft thoughtful follow up messages to the buyer and the tools to re-engage. This creates advocacy and referrals such as tell a friend, social media sharing and posted reviews.</p>
       </div>
     </div> 
 
   </section>
 
-  <section class="section--cta-block">
+  <section class="section--cta-block no-top-padding">
     <div class="container">
       <h2 class="heading-alt heading-alt--tertiary">We motivate shoppers before they are poised to buy.</h2>
       <div class="flex-div">
