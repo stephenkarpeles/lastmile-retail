b298a2bc85be3af6c703a74fee29128e8a594068
Stephen Karpeles <skarpeles@bluechipww.com>
2018-10-05 14:51:30 -0500

    Framework

---
+++ about.php
@@ -0,0 +1,60 @@
+<?php
+  // Define variables for SEO
+  $pageTitle = 'About Us | Lastmile Retail';
+  $pageDescription = 'Description for this page goes here.';
+  $navPageName = 'about';
+  
+  include('partials/header.php'); 
+?>
+
+  <section class="hero hero--about">
+    <div class="container">
+      <div class="hero__copy">
+        <h1>Request a Free Demo Today</h1>
+        <p>It doesn't cost anything to discover how Lastmile can increase your profits. We are ready to help you find the best solutions for your store.</p>
+      </div>      
+      <div class="hero__icon">
+        <img src="img/icons/icon-funnel.svg" alt="Funnel Icon">
+      </div>
+    </div>
+  </section>
+  
+  <section class="about-intro-block no-bottom-padding">
+    <div class="container">
+      <div class="about-intro-block__image-block">
+        <img src="img/icons/icon-omni-channel-alt.svg" alt="Omni-Channel Icon">
+        <p>We change the game by consolidating all of your local reporting into one modern dashboard that measure engagement down to the specific local product and can even track visits and in-store sales.</p>
+      </div>
+    </div>
+  </section>
+
+  <section class="no-bottom-padding">
+    <div class="container">
+      <h2 class="heading-alt heading-alt--secondary heading-alt--tight">We use location to give you a competitive online marketing advantage.</h2>
+      <p class="paragraph-alt">An unparalleled online to offline shopper experience with high click through rates, industry leading conversion rates and measurable ROI Impact.</p>
+    </div>
+  </section>
+
+  <section class="no-top-padding">
+    <div class="container">
+      <div class="grid">
+        <div class="col col--one-half">
+          Test
+        </div>
+        <div class="col col--one-half">
+          Test
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="section--cta-block no-top-padding">
+    <div class="container">
+      <div class="flex-div">
+        <p>Lastmile's patent-pending Local Motive&trade; Omni-Channel Platform provides your company or brand with a unique set of tools to directly promote local products and services to interested nearby customers.</p>
+        <a class="btn btn--tag">Contact Us</a>
+      <div>
+    </div>
+  </section>
+
+<?php include 'partials/footer.php'; ?>
